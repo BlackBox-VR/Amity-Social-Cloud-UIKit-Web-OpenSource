e5e3b36c96fe5babc26ceb5e03577b769b303b48
Abhishek Mohan <abhishekmohan.am@gmail.com>
2023-09-14 14:20:15 -0400

    hmmm

--- src/chat/components/MessageList/index.js
+++ src/chat/components/MessageList/index.js
@@ -48,7 +48,7 @@ const MessageList = ({ client, channelId }) => {
           {messages.map((message, i) => {
             const isAutoPost = message.tags != null && message.tags.indexOf('autopost') > -1;
             const nextMessage = messages[i + 1];
-            const isConsequent = nextMessage && nextMessage.userId === message.userId && (message.tags == null || message.tags.indexOf('autopost') <= 0);
+            const isConsequent = nextMessage && nextMessage.userId === message.userId && isAutoPost;
             const isIncoming = message.userId !== client.currentUserId;
 
             return (
