bf2d1c8d1f1cc063562fd4b51c3241a5b085c948
Chentao <allwithyou999@gmail.com>
2023-09-28 13:56:44 -0700

    Clean up the code

--- src/social/pages/Search/index.js
+++ src/social/pages/Search/index.js
@@ -1,47 +1,37 @@
-import React, {forwardRef, useState} from 'react';
+import React, { forwardRef, useState } from 'react';
 
-import {Wrapper} from './styles';
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import { Wrapper } from './styles';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import UiKitSocialSearch from '~/social/components/SocialSearch';
 import Followers from '~/social/pages/UserFeed/Followers';
-import {FollowersTabs} from "~/social/pages/UserFeed/Followers/constants";
-import {UserFeedTabs} from "~/social/pages/UserFeed/constants";
-import { useSDK } from '~/core/hooks/useSDK';
+import { FollowersTabs } from '~/social/pages/UserFeed/Followers/constants';
+import { UserFeedTabs } from '~/social/pages/UserFeed/constants';
 
 const SocialSearch = styled(UiKitSocialSearch)`
   background: ${({ theme }) => theme.palette.system.background};
   padding: 0.5rem;
 `;
 
-const Search = forwardRef(
-    (
-        {
-            isLandingPage,
-            userId
-        },
-        ref,
-    ) => {
-        const [followActiveTab, setFollowActiveTab] = useState(FollowersTabs.FOLLOWINGS);
-        const [activeTab, setActiveTab] = useState(UserFeedTabs.FOLLOWERS);
-        
-        return (
-            <Wrapper>
-                <SocialSearch searchBy={['accounts']}/>
-                <Followers
-                    userId={userId}
-                    activeTab={followActiveTab}
-                    setActiveTab={setFollowActiveTab}
-                    setUserFeedTab={setActiveTab}
-                />
-            </Wrapper>
-        );
-    },
-);
+const Search = forwardRef(({ userId }) => {
+  const [followActiveTab, setFollowActiveTab] = useState(FollowersTabs.FOLLOWINGS);
+  const [setActiveTab] = useState(UserFeedTabs.FOLLOWERS);
+
+  return (
+    <Wrapper>
+      <SocialSearch searchBy={['accounts']} />
+      <Followers
+        userId={userId}
+        activeTab={followActiveTab}
+        setActiveTab={setFollowActiveTab}
+        setUserFeedTab={setActiveTab}
+      />
+    </Wrapper>
+  );
+});
 
 Search.propTypes = {
-    isLandingPage: PropTypes.string,
-    userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired,
 };
 
 export default Search;
