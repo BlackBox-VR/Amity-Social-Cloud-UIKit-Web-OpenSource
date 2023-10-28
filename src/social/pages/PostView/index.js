4d0a87fa8c64ed589354dbb5238b31b3afae188a
Abhishek Mohan <abhishekmohan.am@gmail.com>
2023-10-28 15:40:30 -0400

    adding back button

--- src/social/pages/PostView/index.js
+++ src/social/pages/PostView/index.js
@@ -2,12 +2,29 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Post from '~/social/components/post/Post';
 import { Wrapper } from './styles';
+import { BackButton, Header, Title } from '~/social/pages/CategoryCommunities/styles';
+import { useNavigation } from '~/social/providers/NavigationProvider';
 
 const PostView = forwardRef(({ postId }, ref) => {
 
+  const { onBack, lastPage } = useNavigation();
+  
   return (
     // key prop is necessary here, without it this part will never re-render !!!
     <Wrapper>
+      <Header>
+        <BackButton onClick={onBack}>
+          <ArrowLeft height={14} />
+        </BackButton>
+
+        {lastPage.type === PageTypes.NewsFeed && <Title>{'Main Feed'}</Title>}
+
+        {lastPage.type === PageTypes.Search && <Title>{'Search & Communities'}</Title>}
+
+        {lastPage.type === PageTypes.Explore && <Title>{'Explore'}</Title>}
+
+        {lastPage.type === PageTypes.CommunityFeed && <Title>{'Community Feed'}</Title>}
+      </Header>
       <Post
         key={postId}
         postId={postId}
