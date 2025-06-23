71f0673ca44fe5937502483b99a1a445d95d8b94
Mike Heitzmann <mikeheitzmann@gmail.com>
2025-06-23 16:50:56 -0500

    MainLayout can now support a sidebar-less state

--- src/v4/social/layouts/Main/Main.tsx
+++ src/v4/social/layouts/Main/Main.tsx
@@ -2,14 +2,42 @@ import React, { PropsWithChildren } from 'react';
 import styles from './Main.module.css';
 
 type MainLayoutProps = PropsWithChildren<{
-  aside: React.ReactNode;
+  aside?: React.ReactNode;
 }>;
 
 export const MainLayout = ({ aside, children }: MainLayoutProps) => {
+  const hasAside = aside != null && aside !== false;
+
   return (
-    <div className={styles.layout}>
-      <aside className={styles.layout__aside}>{aside}</aside>
-      <main className={styles.layout__main}>{children}</main>
+    <div
+      className={`${styles.layout} ${!hasAside ? styles.layout__noAside : ''}`}
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'grid',
+        overflow: 'hidden',
+        gridTemplateColumns: !hasAside ? '1fr' : 'auto 1fr',
+        background: 'var(--asc-color-background-shade1)',
+        position: 'relative',
+      }}
+    >
+      {hasAside && <aside className={styles.layout__aside}>{aside}</aside>}
+      <main
+        className={styles.layout__main}
+        style={{
+          width: '100%',
+          height: '100%',
+          margin: '0 auto',
+          overflow: 'auto',
+          maxWidth: '37.125rem',
+          padding: '1rem 1.5rem 0',
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+          position: 'relative',
+        }}
+      >
+        {children}
+      </main>
     </div>
   );
 };
