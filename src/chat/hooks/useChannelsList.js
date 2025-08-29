7958683db63fa51b64359c1832968fb984384db0
Mike Heitzmann <mikeheitzmann@gmail.com>
2025-08-29 10:38:59 -0500

    Try this

--- src/chat/hooks/useChannelsList.js
+++ src/chat/hooks/useChannelsList.js
@@ -5,6 +5,7 @@ import useLiveCollection from '~/core/hooks/useLiveCollection';
 
 function useChannelsList(teamName = null) {
   const queryParams = teamName ? { displayName: teamName, filter: ChannelFilter.Member } : { filter: ChannelFilter.Member };
+  console.log(queryParams);
   const [channels, hasMore, loadMore] = useLiveCollection(
     // Note: we can not use SDK sortBy LastActivity option - because by default it uses
     // ASC direction from BE. By default LastCreated is used. It still gives wrong result but it
@@ -14,6 +15,8 @@ function useChannelsList(teamName = null) {
   );
 
   const orderedChannels = useMemo(() => orderBy(channels, 'lastActivity', 'desc'), [channels])
+  
+  console.log(orderedChannels);
 
   return [orderedChannels, hasMore, loadMore];
 }
